646afe019c6a2d7b00265277ff79d0e3b8f7e53e
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-02 17:35:26 +0000

    - Prepped for beta build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'proxy_report';
-$app['version'] = '1.4.1';
+$app['version'] = '1.4.2';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
@@ -33,8 +33,8 @@ $app['controllers']['settings']['title'] = lang('base_settings');
 /////////////////////////////////////////////////////////////////////////////
 
 $app['core_requires'] = array(
-    'app-reports-core >= 1:1.4.1',
-    'app-reports-database-core >= 1:1.4.1',
+    'app-reports-core >= 1:1.4.2',
+    'app-reports-database-core >= 1:1.4.2',
     'app-tasks-core',
 );
 
