df6b8cc7af36fb1688e79a437b2dadc495be87ed
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-01-01 22:33:20 +0000

    - Prepped for beta 1 build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'proxy_report';
-$app['version'] = '1.4.8';
+$app['version'] = '1.4.14';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
