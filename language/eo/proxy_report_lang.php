b5df08680d12c7bf9b944116c56c55883f70dc82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:50:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'La Filtro kaj Proxy Raporti disponigas vidon de ttt uzado via reto.';
+$lang['proxy_report_app_name'] = 'Filtri kaj Proxy Raporti';
+$lang['proxy_report_blacklist'] = 'blacklist';
+$lang['proxy_report_blocked'] = 'Blokitaj';
+$lang['proxy_report_bytes'] = 'bitokoj';
+$lang['proxy_report_cache_hits'] = 'kaŝmemoro Trafoj';
+$lang['proxy_report_cache_percentage'] = 'Kaŝaĵo %';
+$lang['proxy_report_hits'] = 'Trafoj';
+$lang['proxy_report_incidents'] = 'incidentoj';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP Resumo';
+$lang['proxy_report_malware_details'] = 'malware Detaloj';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Prokurilo Raporti Dashboard';
+$lang['proxy_report_site'] = 'ejo';
+$lang['proxy_report_sites'] = 'lokoj';
+$lang['proxy_report_site_summary'] = 'ejo Resumo';
+$lang['proxy_report_size'] = 'grandeco';
+$lang['proxy_report_top_ips'] = 'supro IPs';
+$lang['proxy_report_top_sites'] = 'top Sites';
+$lang['proxy_report_top_users'] = 'supro Uzantoj';
+$lang['proxy_report_traffic_summary'] = 'trafiko Resumo';
+$lang['proxy_report_user_summary'] = 'uzanto Resumo';
+$lang['proxy_report_warning_summary'] = 'Averto Resumo';
+$lang['proxy_report_warnings'] = 'avertoj';
