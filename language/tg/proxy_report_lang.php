235dafc2e48d46f0f754b7fc64336cae44037864
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:59:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Дар Филтр ва ваколатнома Њисобот оид ба мақсади истифодаи интернетро дар шабакаи шумо пешкаш мекунад.';
+$lang['proxy_report_app_name'] = 'Полоиш ва ваколатнома Њисобот оид ба';
+$lang['proxy_report_blacklist'] = 'феҳрасте';
+$lang['proxy_report_blocked'] = 'баста';
+$lang['proxy_report_bytes'] = 'байт';
+$lang['proxy_report_cache_hits'] = 'Боздидҳо кеш';
+$lang['proxy_report_cache_percentage'] = 'кеш%';
+$lang['proxy_report_hits'] = 'Боздидҳо';
+$lang['proxy_report_incidents'] = 'ҳаводис';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'IP мухтасари';
+$lang['proxy_report_malware_details'] = 'Тафсилот зараровар';
+$lang['proxy_report_malware'] = 'зараровар';
+$lang['proxy_report_proxy_report_dashboard'] = 'Ваколатномаи Њисобот оид ба панели';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'сомонаҳои';
+$lang['proxy_report_site_summary'] = 'Мазмуни мухтасари сайт';
+$lang['proxy_report_size'] = 'андоза';
+$lang['proxy_report_top_ips'] = 'IPS Top';
+$lang['proxy_report_top_sites'] = 'ТОП сайтҳо';
+$lang['proxy_report_top_users'] = 'Истифодабарандагон дар Top';
+$lang['proxy_report_traffic_summary'] = 'Мазмуни мухтасари трафик';
+$lang['proxy_report_user_summary'] = 'Мазмуни мухтасари корбар';
+$lang['proxy_report_warning_summary'] = 'Мазмуни мухтасари Огоҳӣ';
+$lang['proxy_report_warnings'] = 'огоҳиҳои';
