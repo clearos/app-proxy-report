9e191f1dbcb92bd5dcb720c1f1d231d02cd9fc59
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:54:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ಫಿಲ್ಟರ್ ಮತ್ತು ಪ್ರಾಕ್ಸಿ ವರದಿ ನಿಮ್ಮ ನೆಟ್ವರ್ಕ್ನಲ್ಲಿ ವೆಬ್ ಬಳಕೆಯ ಒಂದು ನೋಟವನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['proxy_report_app_name'] = 'ಫಿಲ್ಟರ್ ಮತ್ತು ಪ್ರಾಕ್ಸಿ ವರದಿ';
+$lang['proxy_report_blacklist'] = 'ಕಪ್ಪುಪಟ್ಟಿ';
+$lang['proxy_report_blocked'] = 'ನಿರ್ಬಂಧಿಸಿದ';
+$lang['proxy_report_bytes'] = 'ಬೈಟ್ಗಳು';
+$lang['proxy_report_cache_hits'] = 'ಸಂಗ್ರಹ ಹಿಟ್ಸ್';
+$lang['proxy_report_cache_percentage'] = 'ಸಂಗ್ರಹ%';
+$lang['proxy_report_hits'] = 'ಹಿಟ್ಸ್';
+$lang['proxy_report_incidents'] = 'ಘಟನೆಗಳು';
+$lang['proxy_report_ips'] = 'ಐಪಿಎಸ್';
+$lang['proxy_report_ip_summary'] = 'ಐಪಿ ಸಾರಾಂಶ';
+$lang['proxy_report_malware_details'] = 'ಮಾಲ್ವೇರ್ ವಿವರಗಳು';
+$lang['proxy_report_malware'] = 'ಮಾಲ್ವೇರ್';
+$lang['proxy_report_proxy_report_dashboard'] = 'ಪ್ರಾಕ್ಸಿ ವರದಿ ಡ್ಯಾಶ್ಬೋರ್ಡ್';
+$lang['proxy_report_site'] = 'ಸೈಟ್';
+$lang['proxy_report_sites'] = 'ಸೈಟ್ಗಳು';
+$lang['proxy_report_site_summary'] = 'ಸೈಟ್ ಸಾರಾಂಶ';
+$lang['proxy_report_size'] = 'ಗಾತ್ರ';
+$lang['proxy_report_top_ips'] = 'ಟಾಪ್ ಐಪಿಎಸ್';
+$lang['proxy_report_top_sites'] = 'ಟಾಪ್ ತಾಣಗಳು';
+$lang['proxy_report_top_users'] = 'ಅಗ್ರ ಬಳಕೆದಾರರು';
+$lang['proxy_report_traffic_summary'] = 'ಸಂಚಾರ ಸಾರಾಂಶ';
+$lang['proxy_report_user_summary'] = 'ಬಳಕೆದಾರ ಸಾರಾಂಶ';
+$lang['proxy_report_warning_summary'] = 'ಎಚ್ಚರಿಕೆ ಸಾರಾಂಶ';
+$lang['proxy_report_warnings'] = 'ಎಚ್ಚರಿಕೆಗಳನ್ನು';
