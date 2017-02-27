49ef82caa09a5d0b883e0fc9e5e4d3537e721d7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:00:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ฟิลเตอร์และพร็อกซีรายงานให้มุมมองของการใช้งานเว็บในเครือข่ายของคุณ';
+$lang['proxy_report_app_name'] = 'กรองและรายงานพร็อกซี่';
+$lang['proxy_report_blacklist'] = 'บัญชีดำ';
+$lang['proxy_report_blocked'] = 'อุดตัน';
+$lang['proxy_report_bytes'] = 'ไบต์';
+$lang['proxy_report_cache_hits'] = 'ฮิตแคช';
+$lang['proxy_report_cache_percentage'] = 'แคช%';
+$lang['proxy_report_hits'] = 'ฮิต';
+$lang['proxy_report_incidents'] = 'เหตุการณ์ที่เกิดขึ้น';
+$lang['proxy_report_ips'] = 'ไอพี';
+$lang['proxy_report_ip_summary'] = 'IP ข้อมูลอย่างย่อ';
+$lang['proxy_report_malware_details'] = 'รายละเอียดมัลแวร์';
+$lang['proxy_report_malware'] = 'มัลแวร์';
+$lang['proxy_report_proxy_report_dashboard'] = 'พร็อกซีรายงานแดชบอร์ด';
+$lang['proxy_report_site'] = 'เว็บไซต์';
+$lang['proxy_report_sites'] = 'เว็บไซต์';
+$lang['proxy_report_site_summary'] = 'ข้อมูลอย่างย่อเว็บไซต์';
+$lang['proxy_report_size'] = 'ขนาด';
+$lang['proxy_report_top_ips'] = 'IP ที่ด้านบน';
+$lang['proxy_report_top_sites'] = 'บนเว็บไซต์';
+$lang['proxy_report_top_users'] = 'ผู้ใช้สูงสุด';
+$lang['proxy_report_traffic_summary'] = 'การเข้าชมข้อมูลอย่างย่อ';
+$lang['proxy_report_user_summary'] = 'สรุปผู้ใช้';
+$lang['proxy_report_warning_summary'] = 'คำเตือนอย่างย่อ';
+$lang['proxy_report_warnings'] = 'คำเตือน';
