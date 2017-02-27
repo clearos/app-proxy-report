10742a12224e8106edeb8a11ce82bee12f92f2ff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:48:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filter və Proxy Report şəbəkə web istifadə bir görünüşü təmin edir.';
+$lang['proxy_report_app_name'] = 'Filter və Proxy Report';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'Ban';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Cache Hits';
+$lang['proxy_report_cache_percentage'] = 'Gizli yer %';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'Hadisələr';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'IP Xülasə';
+$lang['proxy_report_malware_details'] = 'Zərərli proqram Details';
+$lang['proxy_report_malware'] = 'Zərərli proqram';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'Sayt';
+$lang['proxy_report_sites'] = 'Saytlar';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'boy';
+$lang['proxy_report_top_ips'] = 'Top IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top İstifadəçilər';
+$lang['proxy_report_traffic_summary'] = 'Traffic Xülasə';
+$lang['proxy_report_user_summary'] = 'İstifadəçi Xülasə';
+$lang['proxy_report_warning_summary'] = 'Warning Xülasə';
+$lang['proxy_report_warnings'] = 'Uyarılar';
