239d41a83e399a06f5c3a6f3f79087a01dd47baf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:54:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'តម្រងនិងរបាយការណ៍ផ្ដល់នូវទិដ្ឋភាពមួយប្រូកស៊ីនៃការប្រើប្រាស់បណ្ដាញនៅលើបណ្ដាញរបស់អ្នក។';
+$lang['proxy_report_app_name'] = 'ត្រងនិងរបាយការណ៍ប្រូកស៊ី';
+$lang['proxy_report_blacklist'] = 'បញ្ជីខ្មៅ';
+$lang['proxy_report_blocked'] = 'បានទប់ស្កាត់';
+$lang['proxy_report_bytes'] = 'បៃ';
+$lang['proxy_report_cache_hits'] = 'ទស្សនាឃ្លាំងសម្ងាត់';
+$lang['proxy_report_cache_percentage'] = 'ឃ្លាំង​សម្ងាត់ %';
+$lang['proxy_report_hits'] = 'ទស្សនា';
+$lang['proxy_report_incidents'] = 'ឧប្បត្តិហេតុ';
+$lang['proxy_report_ips'] = 'ជនជាតិដើមភាគតិច';
+$lang['proxy_report_ip_summary'] = 'សង្ខេប IP របស់';
+$lang['proxy_report_malware_details'] = 'សេចក្ដីលម្អិតមេរោគ';
+$lang['proxy_report_malware'] = 'មេរោគ';
+$lang['proxy_report_proxy_report_dashboard'] = 'ប្រូកស៊ីផ្ទាំងគ្រប់គ្រងរបាយការណ៍';
+$lang['proxy_report_site'] = 'តំបន់បណ្តាញ';
+$lang['proxy_report_sites'] = 'គេហទំព័រ';
+$lang['proxy_report_site_summary'] = 'សង្ខេបតំបន់បណ្តាញ';
+$lang['proxy_report_size'] = 'ទំហំ';
+$lang['proxy_report_top_ips'] = 'ជនជាតិដើមភាគតិចកំពូល';
+$lang['proxy_report_top_sites'] = 'គេហទំព័រខាងលើ';
+$lang['proxy_report_top_users'] = 'អ្នកប្រើកំពូល';
+$lang['proxy_report_traffic_summary'] = 'សង្ខេបចរាចរណ៍';
+$lang['proxy_report_user_summary'] = 'សេចក្តីសង្ខេបរបស់អ្នកប្រើ';
+$lang['proxy_report_warning_summary'] = 'សង្ខេបការព្រមាន';
+$lang['proxy_report_warnings'] = 'ការព្រមាន';
