18c9d79cafbbdfcf15b554b7b3c971d5167c01a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:51:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ફિલ્ટર અને પ્રોક્સી રિપોર્ટ તમારા નેટવર્ક પર વેબ વપરાશ એક દૃશ્ય પૂરી પાડે છે.';
+$lang['proxy_report_app_name'] = 'ફિલ્ટર કરો અને પ્રોક્સી રિપોર્ટ';
+$lang['proxy_report_blacklist'] = 'બ્લેકલિસ્ટ';
+$lang['proxy_report_blocked'] = 'અવરુદ્ધ';
+$lang['proxy_report_bytes'] = 'બાઇટ્સ';
+$lang['proxy_report_cache_hits'] = 'કેશ હિટ્સ';
+$lang['proxy_report_cache_percentage'] = 'કેશ%';
+$lang['proxy_report_hits'] = 'હિટ્સ';
+$lang['proxy_report_incidents'] = 'બનાવો';
+$lang['proxy_report_ips'] = 'થયેલા IP';
+$lang['proxy_report_ip_summary'] = 'IP સારાંશ';
+$lang['proxy_report_malware_details'] = 'મૉલવેર વિગતો';
+$lang['proxy_report_malware'] = 'મૉલવેર';
+$lang['proxy_report_proxy_report_dashboard'] = 'પ્રોક્સી રિપોર્ટ ડેશબોર્ડ';
+$lang['proxy_report_site'] = 'સાઇટ';
+$lang['proxy_report_sites'] = 'સાઇટ્સ';
+$lang['proxy_report_site_summary'] = 'સાઇટ સારાંશ';
+$lang['proxy_report_size'] = 'માપ';
+$lang['proxy_report_top_ips'] = 'ટોચના થયેલા IP';
+$lang['proxy_report_top_sites'] = 'ટોચની સાઇટ્સ';
+$lang['proxy_report_top_users'] = 'ટોચના વપરાશકર્તાઓ';
+$lang['proxy_report_traffic_summary'] = 'ટ્રાફિક સારાંશ';
+$lang['proxy_report_user_summary'] = 'વપરાશકર્તા સારાંશ';
+$lang['proxy_report_warning_summary'] = 'ચેતવણી સારાંશ';
+$lang['proxy_report_warnings'] = 'ચેતવણી';
