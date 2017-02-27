673e7293dca284ed10f8edba1601b5bc6115b514
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:58:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'The Filter iyo badalka Report bixisaa view of a isticmaalka web on your network.';
+$lang['proxy_report_app_name'] = 'Sifeey iyo badalka Report';
+$lang['proxy_report_blacklist'] = 'liiska madoow';
+$lang['proxy_report_blocked'] = 'Four';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Hits khasnado';
+$lang['proxy_report_cache_percentage'] = 'khasnado%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'Dhacdooyinka';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'IP Kooban';
+$lang['proxy_report_malware_details'] = 'Details furin';
+$lang['proxy_report_malware'] = 'furin';
+$lang['proxy_report_proxy_report_dashboard'] = 'Badalka Report Dashboard';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'Goobaha';
+$lang['proxy_report_site_summary'] = 'Summary Site';
+$lang['proxy_report_size'] = 'size';
+$lang['proxy_report_top_ips'] = 'IPS Top';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Users Top';
+$lang['proxy_report_traffic_summary'] = 'Soo koobid Traffic';
+$lang['proxy_report_user_summary'] = 'User Kooban';
+$lang['proxy_report_warning_summary'] = 'Summary Digniin';
+$lang['proxy_report_warnings'] = 'Digniinaha';
