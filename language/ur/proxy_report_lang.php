041abf12ce7d484beb9f925495cae2a32455950f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:00:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'فلٹر اور پراکسی رپورٹ آپ کے نیٹ ورک پر ویب کے استعمال کی ایک نقطہ نظر فراہم.';
+$lang['proxy_report_app_name'] = 'فلٹر اور پراکسی رپورٹ';
+$lang['proxy_report_blacklist'] = 'بلیک لسٹ';
+$lang['proxy_report_blocked'] = 'کریں';
+$lang['proxy_report_bytes'] = 'بائٹس';
+$lang['proxy_report_cache_hits'] = 'کیشے مشاہدات';
+$lang['proxy_report_cache_percentage'] = 'کیشے٪';
+$lang['proxy_report_hits'] = 'مشاہدات';
+$lang['proxy_report_incidents'] = 'واقعات';
+$lang['proxy_report_ips'] = 'آئی پی ایس';
+$lang['proxy_report_ip_summary'] = 'IP خلاصہ';
+$lang['proxy_report_malware_details'] = 'میلویئر کی تفصیلات';
+$lang['proxy_report_malware'] = 'میلویئر';
+$lang['proxy_report_proxy_report_dashboard'] = 'پراکسی رپورٹ ڈیش بورڈ';
+$lang['proxy_report_site'] = 'سائٹ';
+$lang['proxy_report_sites'] = 'سائٹس';
+$lang['proxy_report_site_summary'] = 'ویب سائٹ کا خلاصہ';
+$lang['proxy_report_size'] = 'سائز';
+$lang['proxy_report_top_ips'] = 'اوپر آئی پی ایس';
+$lang['proxy_report_top_sites'] = 'اوپر سائٹس';
+$lang['proxy_report_top_users'] = 'اوپر صارفین';
+$lang['proxy_report_traffic_summary'] = 'ٹریفک کا خلاصہ';
+$lang['proxy_report_user_summary'] = 'رکن کا خلاصہ';
+$lang['proxy_report_warning_summary'] = 'انتباہ خلاصہ';
+$lang['proxy_report_warnings'] = 'انتباہ';
