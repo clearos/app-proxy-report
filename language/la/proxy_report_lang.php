3397037d76482824c1dbb8da233e2be3389c51a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:54:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Ergo et usus filter Relatio autem in telam network praebet prospectum.';
+$lang['proxy_report_app_name'] = 'Ergo et Filter Report';
+$lang['proxy_report_blacklist'] = 'blacklist';
+$lang['proxy_report_blocked'] = 'clausus';
+$lang['proxy_report_bytes'] = 'MB';
+$lang['proxy_report_cache_hits'] = 'Album cache';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'Album';
+$lang['proxy_report_incidents'] = 'rebus';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'Libri IV';
+$lang['proxy_report_malware_details'] = 'Details malware';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Ergo Dashboard Report';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'sites';
+$lang['proxy_report_site_summary'] = 'Summary Site';
+$lang['proxy_report_size'] = 'Size';
+$lang['proxy_report_top_ips'] = 'Top IPS';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top';
+$lang['proxy_report_traffic_summary'] = 'Summary dolor';
+$lang['proxy_report_user_summary'] = 'User Summary';
+$lang['proxy_report_warning_summary'] = 'Purchase monito';
+$lang['proxy_report_warnings'] = 'warnings';
