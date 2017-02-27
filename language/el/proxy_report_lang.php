912d6728cf7b58333085fcc466f306abf24c9f58
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:51:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Το φίλτρο και μεσολάβησης έκθεση παρέχει μια προβολή της χρήσης ιστοσελίδων στο δίκτυό σας.';
+$lang['proxy_report_app_name'] = 'Φιλτράρει και μεσολάβησης Έκθεση';
+$lang['proxy_report_blacklist'] = 'Προγραφή';
+$lang['proxy_report_blocked'] = 'Αποκλεισμένοι';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Επισκέψεις Cache';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'Επισκέψεις';
+$lang['proxy_report_incidents'] = 'επεισόδια';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'Περίληψη IP';
+$lang['proxy_report_malware_details'] = 'malware Λεπτομέρειες';
+$lang['proxy_report_malware'] = 'Κακόβουλο λογισμικό';
+$lang['proxy_report_proxy_report_dashboard'] = 'Μεσολάβησης ταμπλό Έκθεση';
+$lang['proxy_report_site'] = 'Ιστοσελίδα';
+$lang['proxy_report_sites'] = 'τοποθεσίες';
+$lang['proxy_report_site_summary'] = 'Περίληψη ιστοσελίδα';
+$lang['proxy_report_size'] = 'Μέγεθος';
+$lang['proxy_report_top_ips'] = 'Top IPs';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top χρήστες';
+$lang['proxy_report_traffic_summary'] = 'Περίληψη της κυκλοφορίας';
+$lang['proxy_report_user_summary'] = 'Περίληψη χρήστη';
+$lang['proxy_report_warning_summary'] = 'Περίληψη προειδοποίηση';
+$lang['proxy_report_warnings'] = 'προειδοποιήσεις';
