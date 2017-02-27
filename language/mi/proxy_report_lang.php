00428d464268ee7d6bbe0a0cc4203fbbffc4f6c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:55:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Te Tātari ko Report Takawaenga whakarato he tirohanga o te whakamahi tukutuku i runga i tou whatunga.';
+$lang['proxy_report_app_name'] = 'Tātari me te Pūrongo Takawaenga';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'āraia';
+$lang['proxy_report_bytes'] = 'Paita';
+$lang['proxy_report_cache_hits'] = 'Hits Keteroki';
+$lang['proxy_report_cache_percentage'] = 'Keteroki%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'mahinga';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'Whakarāpopototanga IP';
+$lang['proxy_report_malware_details'] = 'Ngā Taipitopito pūmanawa';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Takawaenga Report Papatohu';
+$lang['proxy_report_site'] = 'pae';
+$lang['proxy_report_sites'] = 'pae';
+$lang['proxy_report_site_summary'] = 'Whakarāpopoto pae';
+$lang['proxy_report_size'] = 'rahi';
+$lang['proxy_report_top_ips'] = 'IP Top';
+$lang['proxy_report_top_sites'] = 'Pae Top';
+$lang['proxy_report_top_users'] = 'Kaiwhakamahi Top';
+$lang['proxy_report_traffic_summary'] = 'Whakarāpopoto Traffic';
+$lang['proxy_report_user_summary'] = 'Whakarāpopoto kaiwhakamahi';
+$lang['proxy_report_warning_summary'] = 'Whakarāpopoto whakatūpato';
+$lang['proxy_report_warnings'] = 'faaararaa';
