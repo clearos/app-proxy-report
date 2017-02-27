78513ced9de45481bbb1795ce0109542149c04a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:53:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'The Filter og Proxy Report veitir útsýni yfir notkun vefur á netið.';
+$lang['proxy_report_app_name'] = 'Sía og Proxy Report';
+$lang['proxy_report_blacklist'] = 'Svartur listi';
+$lang['proxy_report_blocked'] = 'Bannaður';
+$lang['proxy_report_bytes'] = 'bæti';
+$lang['proxy_report_cache_hits'] = 'Cache Hits';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'atvik';
+$lang['proxy_report_ips'] = 'IP-tölum';
+$lang['proxy_report_ip_summary'] = 'IP Yfirlit';
+$lang['proxy_report_malware_details'] = 'malware Upplýsingar';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Mælaborð';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'síður';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Size';
+$lang['proxy_report_top_ips'] = 'Top IP-tölum';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Helstu notendur';
+$lang['proxy_report_traffic_summary'] = 'Samantekt Traffic';
+$lang['proxy_report_user_summary'] = 'User Yfirlit';
+$lang['proxy_report_warning_summary'] = 'Viðvörun Yfirlit';
+$lang['proxy_report_warnings'] = 'Viðvaranir';
