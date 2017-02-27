7a66103c3b641dd9e4214e33f2b0c37947202f8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:54:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filtras ir proxy ataskaita suteikia interneto naudojimo požiūrį į savo tinklą.';
+$lang['proxy_report_app_name'] = 'Filtruoti ir proxy ataskaita';
+$lang['proxy_report_blacklist'] = 'juodasis sąrašas';
+$lang['proxy_report_blocked'] = 'Užblokuoti';
+$lang['proxy_report_bytes'] = 'baitų';
+$lang['proxy_report_cache_hits'] = 'cache hitai';
+$lang['proxy_report_cache_percentage'] = 'Laikinoji%';
+$lang['proxy_report_hits'] = 'hitai';
+$lang['proxy_report_incidents'] = 'incidentai';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'IP santrauka';
+$lang['proxy_report_malware_details'] = 'kenkėjiškų detalės';
+$lang['proxy_report_malware'] = 'kenkėjiškų programų';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy ataskaita skydelis';
+$lang['proxy_report_site'] = 'vieta';
+$lang['proxy_report_sites'] = 'Sklypai';
+$lang['proxy_report_site_summary'] = 'Svetainės suvestinė';
+$lang['proxy_report_size'] = 'dydis';
+$lang['proxy_report_top_ips'] = 'Geriausi IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Aktyviausi vartotojai';
+$lang['proxy_report_traffic_summary'] = 'eismo santrauka';
+$lang['proxy_report_user_summary'] = 'vartotojas santrauka';
+$lang['proxy_report_warning_summary'] = 'Įspėjimas santrauka';
+$lang['proxy_report_warnings'] = 'įspėjimai';
