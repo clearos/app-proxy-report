16456366dc6e2138ef911f0d453860582c7c8190
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:50:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filter ja Proxy aruanne pakub vaade veebi kasutamine võrgu.';
+$lang['proxy_report_app_name'] = 'Filter ja Proxy aruanne';
+$lang['proxy_report_blacklist'] = 'must nimekiri';
+$lang['proxy_report_blocked'] = 'blokeeritud';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'Juhtumid';
+$lang['proxy_report_ips'] = 'IP-aadressi';
+$lang['proxy_report_ip_summary'] = 'IP kokkuvõte';
+$lang['proxy_report_malware_details'] = 'Malware detailid';
+$lang['proxy_report_malware'] = 'Malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy aruanne Armatuurlaud';
+$lang['proxy_report_site'] = 'Saidi';
+$lang['proxy_report_sites'] = 'saidid';
+$lang['proxy_report_site_summary'] = 'Saidi kokkuvõte';
+$lang['proxy_report_size'] = 'suurus';
+$lang['proxy_report_top_ips'] = 'Top IP-aadressi';
+$lang['proxy_report_top_sites'] = 'Top saidid';
+$lang['proxy_report_top_users'] = 'Top Kasutajad';
+$lang['proxy_report_traffic_summary'] = 'Traffic kokkuvõte';
+$lang['proxy_report_user_summary'] = 'Kasutaja kokkuvõte';
+$lang['proxy_report_warning_summary'] = 'Hoiatus kokkuvõte';
+$lang['proxy_report_warnings'] = 'Hoiatused';
