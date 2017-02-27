f783b53cc1c07d22bf16dc3be492e9c1692ea771
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:59:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filter na Ripoti Wakala inatoa mtazamo wa matumizi ya mtandao kwenye mtandao wako.';
+$lang['proxy_report_app_name'] = 'Kuchuja na Ripoti Wakala';
+$lang['proxy_report_blacklist'] = 'blacklist';
+$lang['proxy_report_blocked'] = 'imefungwa';
+$lang['proxy_report_bytes'] = 'ka';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'hits';
+$lang['proxy_report_incidents'] = 'matukio ya';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'IP Summary';
+$lang['proxy_report_malware_details'] = 'Maelezo Malware';
+$lang['proxy_report_malware'] = 'Malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Wakala Ripoti Dashboard';
+$lang['proxy_report_site'] = 'tovuti';
+$lang['proxy_report_sites'] = 'maeneo';
+$lang['proxy_report_site_summary'] = 'site Summary';
+$lang['proxy_report_size'] = 'ukubwa';
+$lang['proxy_report_top_ips'] = 'juu IPS';
+$lang['proxy_report_top_sites'] = 'juu Sites';
+$lang['proxy_report_top_users'] = 'Watumiaji wakuu';
+$lang['proxy_report_traffic_summary'] = 'trafiki Summary';
+$lang['proxy_report_user_summary'] = 'Summary mtumiaji';
+$lang['proxy_report_warning_summary'] = 'onyo Summary';
+$lang['proxy_report_warnings'] = 'onyo';
