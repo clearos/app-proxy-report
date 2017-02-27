47a0ba62f52fcc2830c639318ca3a287536dd11f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:01:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Bộ lọc và Proxy Báo cáo cung cấp một cái nhìn của việc sử dụng web trên mạng của bạn.';
+$lang['proxy_report_app_name'] = 'Lọc và Proxy Báo cáo';
+$lang['proxy_report_blacklist'] = 'Danh sách đen';
+$lang['proxy_report_blocked'] = 'Bị chặn';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Số lượt truy cập bộ nhớ cache';
+$lang['proxy_report_cache_percentage'] = 'bộ nhớ cache%';
+$lang['proxy_report_hits'] = 'Số lượt truy cập';
+$lang['proxy_report_incidents'] = 'sự cố';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'Tóm tắt IP';
+$lang['proxy_report_malware_details'] = 'Chi tiết phần mềm độc hại';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Dashboard Báo cáo';
+$lang['proxy_report_site'] = 'nơi';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'site Summary';
+$lang['proxy_report_size'] = 'Kích thước';
+$lang['proxy_report_top_ips'] = 'Lên trên IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Về đầu Trang';
+$lang['proxy_report_traffic_summary'] = 'Tóm tắt giao thông';
+$lang['proxy_report_user_summary'] = 'Tóm tắt tài';
+$lang['proxy_report_warning_summary'] = 'Tóm tắt cảnh báo';
+$lang['proxy_report_warnings'] = 'cảnh báo';
