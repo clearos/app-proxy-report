e2cce035932412769f99660d9fde85618e5cf306
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:01:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'די פֿילטריר און פּראָקסי באריכט גיט אַ קוק פון וועב באַניץ אויף דיין נעץ.';
+$lang['proxy_report_app_name'] = 'פֿילטריר און פּראָקסי באריכט';
+$lang['proxy_report_blacklist'] = 'בלאַקקליסט';
+$lang['proxy_report_blocked'] = 'בלאַקט';
+$lang['proxy_report_bytes'] = 'ביטעס';
+$lang['proxy_report_cache_hits'] = 'קאַטשע היץ';
+$lang['proxy_report_cache_percentage'] = 'קאַטשע%';
+$lang['proxy_report_hits'] = 'היץ';
+$lang['proxy_report_incidents'] = 'ינסידענץ';
+$lang['proxy_report_ips'] = 'יפּס';
+$lang['proxy_report_ip_summary'] = 'יפּ סאַמערי';
+$lang['proxy_report_malware_details'] = 'מאַלוואַרע דעטאַילס';
+$lang['proxy_report_malware'] = 'מאַלוואַרע';
+$lang['proxy_report_proxy_report_dashboard'] = 'פּראָקסי באריכט דאַשבאָאַרד';
+$lang['proxy_report_site'] = 'פּלאַץ';
+$lang['proxy_report_sites'] = 'זייטלעך';
+$lang['proxy_report_site_summary'] = 'מאַפּע סאַמערי';
+$lang['proxy_report_size'] = 'גרייס';
+$lang['proxy_report_top_ips'] = 'שפּיץ יפּס';
+$lang['proxy_report_top_sites'] = 'שפּיץ זייטלעך';
+$lang['proxy_report_top_users'] = 'שפּיץ ניצערס';
+$lang['proxy_report_traffic_summary'] = 'פאַרקער קיצער';
+$lang['proxy_report_user_summary'] = 'באַניצער סאַמערי';
+$lang['proxy_report_warning_summary'] = 'ווארענונג סאַמערי';
+$lang['proxy_report_warnings'] = 'וואַרנינגס';
