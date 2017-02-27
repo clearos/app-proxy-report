b2d32c90f0c9ab8d1a882795190596f768bb55aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:59:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ఫిల్టర్ మరియు ప్రాక్సీ నివేదిక మీ నెట్వర్క్లో వెబ్ వినియోగ యొక్క కోణాన్ని అందిస్తుంది.';
+$lang['proxy_report_app_name'] = 'వడపోసి ప్రాక్సీ నివేదిక';
+$lang['proxy_report_blacklist'] = 'బ్లాక్లిస్ట్';
+$lang['proxy_report_blocked'] = 'నిరోధిత';
+$lang['proxy_report_bytes'] = 'బైట్లు';
+$lang['proxy_report_cache_hits'] = 'కాష్ హిట్స్';
+$lang['proxy_report_cache_percentage'] = 'కాష్%';
+$lang['proxy_report_hits'] = 'హిట్స్';
+$lang['proxy_report_incidents'] = 'సంఘటనలు';
+$lang['proxy_report_ips'] = 'IP లు';
+$lang['proxy_report_ip_summary'] = 'ఐపీ సారాంశం';
+$lang['proxy_report_malware_details'] = 'మాల్వేర్ వివరాలు';
+$lang['proxy_report_malware'] = 'మాల్వేర్';
+$lang['proxy_report_proxy_report_dashboard'] = 'ప్రాక్సీ నివేదిక డాష్బోర్డ్';
+$lang['proxy_report_site'] = 'సైట్';
+$lang['proxy_report_sites'] = 'సైట్లు';
+$lang['proxy_report_site_summary'] = 'సైట్ సమ్మరీ';
+$lang['proxy_report_size'] = 'పరిమాణం';
+$lang['proxy_report_top_ips'] = 'టాప్ IP లు';
+$lang['proxy_report_top_sites'] = 'టాప్ సైట్స్';
+$lang['proxy_report_top_users'] = 'అగ్ర యూజర్లు';
+$lang['proxy_report_traffic_summary'] = 'ట్రాఫిక్ సారాంశం';
+$lang['proxy_report_user_summary'] = 'వాడుకరి సారాంశం';
+$lang['proxy_report_warning_summary'] = 'హెచ్చరిక సారాంశం';
+$lang['proxy_report_warnings'] = 'హెచ్చరికలు';
