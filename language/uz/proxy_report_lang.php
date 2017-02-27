77dbc6780dff87261ae01515f116a34c2a70f5cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:01:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filter va Proksi hisoboti sizning tarmoq veb foydalanish bir ko&#39;rinishini beradi.';
+$lang['proxy_report_app_name'] = 'Filtrlaydigan va Proksi Hisobot';
+$lang['proxy_report_blacklist'] = 'Filtr-Qora ro&#39;yxat';
+$lang['proxy_report_blocked'] = 'bloklangan';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Kesh Hits';
+$lang['proxy_report_cache_percentage'] = 'Kesh%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'hodisalar';
+$lang['proxy_report_ips'] = 'ilmim';
+$lang['proxy_report_ip_summary'] = 'IP Xulosa';
+$lang['proxy_report_malware_details'] = 'Malware Tafsilotlar';
+$lang['proxy_report_malware'] = 'Malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proksi Hisobot Dashboard';
+$lang['proxy_report_site'] = 'sayt';
+$lang['proxy_report_sites'] = 'saytlar';
+$lang['proxy_report_site_summary'] = 'sayt Xulosa';
+$lang['proxy_report_size'] = 'Size';
+$lang['proxy_report_top_ips'] = 'Top-ilmim';
+$lang['proxy_report_top_sites'] = 'Eng yaxshi Saytlari';
+$lang['proxy_report_top_users'] = 'Top foydalanuvchilar';
+$lang['proxy_report_traffic_summary'] = 'Traffic Xulosa';
+$lang['proxy_report_user_summary'] = 'foydalanuvchi Xulosa';
+$lang['proxy_report_warning_summary'] = 'ogohlantirish Xulosa';
+$lang['proxy_report_warnings'] = 'ogohlantirishlar';
