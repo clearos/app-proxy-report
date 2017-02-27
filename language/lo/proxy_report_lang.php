349589d87444748051eacbfbbc7cff17377b8a62
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:54:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ກອງແລະບົດລາຍງານ Proxy ໃຫ້ທັດສະນະຂອງການນໍາໃຊ້ເວັບໄຊຕ໌ໃນເຄືອຂ່າຍຂອງທ່ານ.';
+$lang['proxy_report_app_name'] = 'ການກັ່ນຕອງແລະບົດລາຍງານ Proxy';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'ສະກັດ';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Hits Cache';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'ເຫດການ';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP Summary';
+$lang['proxy_report_malware_details'] = 'ລາຍລະອຽດ malware';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'ເວັບໄຊ';
+$lang['proxy_report_sites'] = 'ສະຖານທີ່';
+$lang['proxy_report_site_summary'] = 'ສະຫຼຸບທີ່ຕັ້ງ';
+$lang['proxy_report_size'] = 'ຂະຫນາດ';
+$lang['proxy_report_top_ips'] = 'IPs Top';
+$lang['proxy_report_top_sites'] = 'ສະຖານທີ່ຫນ້າສົນໃຈ';
+$lang['proxy_report_top_users'] = 'ຂ້າງເທິງຜູ້ທີ່';
+$lang['proxy_report_traffic_summary'] = 'ການຈະລາຈອນ Summary';
+$lang['proxy_report_user_summary'] = 'Summary ຜູ້ໃຊ້';
+$lang['proxy_report_warning_summary'] = 'ການເຕືອນໄພ Summary';
+$lang['proxy_report_warnings'] = 'ການເຕືອນໄພ';
