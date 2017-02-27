d0d3255503ccd5a6fe89866db994eaea991c6a1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:49:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ফিল্টার এবং প্রক্সি প্রতিবেদন আপনার নেটওয়ার্কে ওয়েব ব্যবহার একটি দৃশ্য প্রদান করে.';
+$lang['proxy_report_app_name'] = 'ফিল্টার এবং প্রক্সি প্রতিবেদন';
+$lang['proxy_report_blacklist'] = 'নিষিদ্ধ জিনিসের তালিকা';
+$lang['proxy_report_blocked'] = 'অবরুদ্ধ';
+$lang['proxy_report_bytes'] = 'বাইট';
+$lang['proxy_report_cache_hits'] = 'ক্যাশে হিট';
+$lang['proxy_report_cache_percentage'] = 'ক্যাশে%';
+$lang['proxy_report_hits'] = 'আঘাত';
+$lang['proxy_report_incidents'] = 'ঘটনা';
+$lang['proxy_report_ips'] = 'চাকরি';
+$lang['proxy_report_ip_summary'] = 'আইপি সংক্ষিপ্তসার';
+$lang['proxy_report_malware_details'] = 'Malware সম্পর্কে বিস্তারিত';
+$lang['proxy_report_malware'] = 'Malware সম্পর্কে';
+$lang['proxy_report_proxy_report_dashboard'] = 'প্রক্সি প্রতিবেদন ড্যাশবোর্ড';
+$lang['proxy_report_site'] = 'সাইট';
+$lang['proxy_report_sites'] = 'সাইট';
+$lang['proxy_report_site_summary'] = 'সাইট সামারি';
+$lang['proxy_report_size'] = 'আয়তন';
+$lang['proxy_report_top_ips'] = 'শীর্ষ আইপিগুলি';
+$lang['proxy_report_top_sites'] = 'শীর্ষ সাইট';
+$lang['proxy_report_top_users'] = 'প্রজন্ম ডট কম';
+$lang['proxy_report_traffic_summary'] = 'ট্রাফিক সংক্ষিপ্তসার';
+$lang['proxy_report_user_summary'] = 'ব্যবহারকারী সংক্ষিপ্তসার';
+$lang['proxy_report_warning_summary'] = 'সতর্কতা সংক্ষিপ্তসার';
+$lang['proxy_report_warnings'] = 'সতর্কবাণী';
