0b49cf5f9875257aae12e8b4f7c34abdcadd51a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:57:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'فیلتر و پروکسی گزارش نمایی از استفاده از وب بر روی شبکه خود را فراهم می کند.';
+$lang['proxy_report_app_name'] = 'فیلتر و پروکسی گزارش';
+$lang['proxy_report_blacklist'] = 'لیست سیاه';
+$lang['proxy_report_blocked'] = 'مسدود';
+$lang['proxy_report_bytes'] = 'بایت';
+$lang['proxy_report_cache_hits'] = 'حافظه پنهان';
+$lang['proxy_report_cache_percentage'] = 'کش٪';
+$lang['proxy_report_hits'] = 'بازدید';
+$lang['proxy_report_incidents'] = 'حوادث';
+$lang['proxy_report_ips'] = 'پلیس عراقی';
+$lang['proxy_report_ip_summary'] = 'IP خلاصه';
+$lang['proxy_report_malware_details'] = 'اطلاعات نرم افزارهای مخرب';
+$lang['proxy_report_malware'] = 'بد افزار';
+$lang['proxy_report_proxy_report_dashboard'] = 'پروکسی گزارش داشبورد';
+$lang['proxy_report_site'] = 'سایت';
+$lang['proxy_report_sites'] = 'سایت های';
+$lang['proxy_report_site_summary'] = 'خلاصه سایت';
+$lang['proxy_report_size'] = 'اندازه';
+$lang['proxy_report_top_ips'] = 'IP های بالا';
+$lang['proxy_report_top_sites'] = 'سایت های بالا';
+$lang['proxy_report_top_users'] = 'کاربران برتر';
+$lang['proxy_report_traffic_summary'] = 'خلاصه ترافیک';
+$lang['proxy_report_user_summary'] = 'کاربر خلاصه';
+$lang['proxy_report_warning_summary'] = 'خلاصه هشدار';
+$lang['proxy_report_warnings'] = 'هشدار';
