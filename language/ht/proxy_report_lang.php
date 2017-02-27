6fc840db2b9351a78ce402e42e263e7ec4ec4890
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:52:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filter nan ak Proxy rapò bay yon gade nan l &#39;entènèt sou rezo w lan.';
+$lang['proxy_report_app_name'] = 'Filter ak Proxy Rapò';
+$lang['proxy_report_blacklist'] = 'Lis nwa';
+$lang['proxy_report_blocked'] = 'Blocked';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Cache Hits';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'ensidan';
+$lang['proxy_report_ips'] = 'ONSÈY';
+$lang['proxy_report_ip_summary'] = 'IP Rezime';
+$lang['proxy_report_malware_details'] = 'Detay malveyan';
+$lang['proxy_report_malware'] = 'malveyan';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Rapò Dach';
+$lang['proxy_report_site'] = 'Sit';
+$lang['proxy_report_sites'] = 'Sit';
+$lang['proxy_report_site_summary'] = 'Rezime lokal la';
+$lang['proxy_report_size'] = 'Kantite moun ki';
+$lang['proxy_report_top_ips'] = 'Top ONSÈY';
+$lang['proxy_report_top_sites'] = 'Sit Top';
+$lang['proxy_report_top_users'] = 'Top Itilizatè yo';
+$lang['proxy_report_traffic_summary'] = 'Rezime Trafik';
+$lang['proxy_report_user_summary'] = 'Rezime itilizatè';
+$lang['proxy_report_warning_summary'] = 'Avètisman Rezime';
+$lang['proxy_report_warnings'] = 'avètisman';
