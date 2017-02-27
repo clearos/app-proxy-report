8a21554b153be4e4fbecefe58148f26a2d57d2b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:01:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Awọn àlẹmọ ati aṣoju Iroyin pese a view of ayelujara lilo lori nẹtiwọki rẹ.';
+$lang['proxy_report_app_name'] = 'Àlẹmọ ati aṣoju Iroyin';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'dina';
+$lang['proxy_report_bytes'] = 'Bytes';
+$lang['proxy_report_cache_hits'] = 'kaṣe deba';
+$lang['proxy_report_cache_percentage'] = 'kaṣe%';
+$lang['proxy_report_hits'] = 'deba';
+$lang['proxy_report_incidents'] = 'awọn iṣẹlẹ';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP Lakotan';
+$lang['proxy_report_malware_details'] = 'malware alaye';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Aṣoju Iroyin Dashboard';
+$lang['proxy_report_site'] = 'ojula';
+$lang['proxy_report_sites'] = 'ojula';
+$lang['proxy_report_site_summary'] = 'Aye Lakotan';
+$lang['proxy_report_size'] = 'iwọn';
+$lang['proxy_report_top_ips'] = 'Top IPs';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top oníṣe';
+$lang['proxy_report_traffic_summary'] = 'Traffic Lakotan';
+$lang['proxy_report_user_summary'] = 'User Lakotan';
+$lang['proxy_report_warning_summary'] = 'ìkìlọ Lakotan';
+$lang['proxy_report_warnings'] = 'ikilo';
