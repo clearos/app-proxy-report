1b27ec78fa4af99e812b2be1499eb1a34814623b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:48:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'تصفية وتقرير وكيل يوفر ضوء استخدام شبكة الإنترنت على شبكة الاتصال.';
+$lang['proxy_report_app_name'] = 'تصفية وتقرير وكيل';
+$lang['proxy_report_blacklist'] = 'القائمة السوداء';
+$lang['proxy_report_blocked'] = 'مسدود';
+$lang['proxy_report_bytes'] = 'بايت';
+$lang['proxy_report_cache_hits'] = 'مخبأ الفعالية';
+$lang['proxy_report_cache_percentage'] = 'مخبأ٪';
+$lang['proxy_report_hits'] = 'الفعالية';
+$lang['proxy_report_incidents'] = 'حوادث';
+$lang['proxy_report_ips'] = 'المتكاملة';
+$lang['proxy_report_ip_summary'] = 'ملخص IP';
+$lang['proxy_report_malware_details'] = 'تفاصيل البرمجيات الخبيثة';
+$lang['proxy_report_malware'] = 'البرمجيات الخبيثة';
+$lang['proxy_report_proxy_report_dashboard'] = 'وكيل تقرير لوحة';
+$lang['proxy_report_site'] = 'موقع';
+$lang['proxy_report_sites'] = 'مواقع';
+$lang['proxy_report_site_summary'] = 'خلاصة الموقع';
+$lang['proxy_report_size'] = 'بحجم';
+$lang['proxy_report_top_ips'] = 'أعلى المتكاملة';
+$lang['proxy_report_top_sites'] = 'أفضل المواقع';
+$lang['proxy_report_top_users'] = 'أعلى الصفحة عدد';
+$lang['proxy_report_traffic_summary'] = 'ملخص حركة المرور';
+$lang['proxy_report_user_summary'] = 'ملخص المستخدم';
+$lang['proxy_report_warning_summary'] = 'ملخص تحذير';
+$lang['proxy_report_warnings'] = 'تحذيرات';
