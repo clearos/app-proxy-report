a1183d634c29b640380cceecb549ba90e9779e2c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:55:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Ny Filter sy ny Proxy Report dia manome ny fomba fijery ny fampiasana aterineto amin&#39;ny tambajotra.';
+$lang['proxy_report_app_name'] = 'Hanivana sy Proxy Report';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'voasakana';
+$lang['proxy_report_bytes'] = 'Bytes';
+$lang['proxy_report_cache_hits'] = 'Cache Hits';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'zava-nitranga';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'IP Summary';
+$lang['proxy_report_malware_details'] = 'malware Details';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Size';
+$lang['proxy_report_top_ips'] = 'Top IPS';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top Users';
+$lang['proxy_report_traffic_summary'] = 'Traffic Summary';
+$lang['proxy_report_user_summary'] = 'User Summary';
+$lang['proxy_report_warning_summary'] = 'fampitandremana Summary';
+$lang['proxy_report_warnings'] = 'Warnings';
