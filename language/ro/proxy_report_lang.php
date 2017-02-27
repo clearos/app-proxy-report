59d2380df4b1760273e1d2a784389d6ae3c9beba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:57:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filtru și Raportul Proxy oferă o imagine de utilizare web de pe rețea.';
+$lang['proxy_report_app_name'] = 'Se filtrează și Raportul Proxy';
+$lang['proxy_report_blacklist'] = 'blacklist';
+$lang['proxy_report_blocked'] = 'blocate';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Hit-uri cache';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'hit-uri';
+$lang['proxy_report_incidents'] = 'incidente';
+$lang['proxy_report_ips'] = 'IP-uri';
+$lang['proxy_report_ip_summary'] = 'Sumar IP';
+$lang['proxy_report_malware_details'] = 'Detalii malware';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy rapoarte Tabloul de bord';
+$lang['proxy_report_site'] = 'teren';
+$lang['proxy_report_sites'] = 'Site-uri';
+$lang['proxy_report_site_summary'] = 'Rezumatul site-ului';
+$lang['proxy_report_size'] = 'mărimea';
+$lang['proxy_report_top_ips'] = 'Top IP-uri';
+$lang['proxy_report_top_sites'] = 'Top Site-uri';
+$lang['proxy_report_top_users'] = 'Top utilizatori';
+$lang['proxy_report_traffic_summary'] = 'Rezumat trafic';
+$lang['proxy_report_user_summary'] = 'Sumar utilizator';
+$lang['proxy_report_warning_summary'] = 'Sumar de avertizare';
+$lang['proxy_report_warnings'] = 'Atenționări';
