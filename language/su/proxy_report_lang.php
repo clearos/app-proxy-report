064b67647d41e2b568e2dae23160504941cc5533
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:59:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Nu Filter jeung Laporan proxy nyadiakeun panempoan pamakéan web dina jaringan Anjeun.';
+$lang['proxy_report_app_name'] = 'Ngayak jeung Laporan proxy';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'diblokir';
+$lang['proxy_report_bytes'] = 'bait';
+$lang['proxy_report_cache_hits'] = 'hits cache';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'hits';
+$lang['proxy_report_incidents'] = 'Kajadian';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'IP Summary';
+$lang['proxy_report_malware_details'] = 'Rincian malware';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Laporan Dashboard';
+$lang['proxy_report_site'] = 'loka';
+$lang['proxy_report_sites'] = 'situs';
+$lang['proxy_report_site_summary'] = 'Summary loka';
+$lang['proxy_report_size'] = 'ukuran';
+$lang['proxy_report_top_ips'] = 'IPS top';
+$lang['proxy_report_top_sites'] = 'loka top';
+$lang['proxy_report_top_users'] = 'Pamaké luhur';
+$lang['proxy_report_traffic_summary'] = 'Summary traffic';
+$lang['proxy_report_user_summary'] = 'Summary pamaké';
+$lang['proxy_report_warning_summary'] = 'Summary Perhatosan';
+$lang['proxy_report_warnings'] = 'warnings';
