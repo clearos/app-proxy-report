b9667dce233f79a18ae43bb47bc0e967f600c815
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:57:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ਫਿਲਟਰ ਅਤੇ ਪਰਾਕਸੀ ਦੀ ਰਿਪੋਰਟ ਆਪਣੇ ਨੈੱਟਵਰਕ &#39;ਤੇ ਵੈੱਬ ਦੇ ਉਪਯੋਗ ਦੇ ਇੱਕ ਝਲਕ ਦਿੰਦਾ ਹੈ.';
+$lang['proxy_report_app_name'] = 'ਫਿਲਟਰ ਅਤੇ ਪਰਾਕਸੀ ਦੀ ਰਿਪੋਰਟ';
+$lang['proxy_report_blacklist'] = 'ਬਲੈਕਲਿਸਟ';
+$lang['proxy_report_blocked'] = 'ਰੋਕੀ';
+$lang['proxy_report_bytes'] = 'ਬਾਈਟ';
+$lang['proxy_report_cache_hits'] = 'ਕੈਸ਼ Hits';
+$lang['proxy_report_cache_percentage'] = 'ਕੈਸ਼%';
+$lang['proxy_report_hits'] = 'ਹਿੱਟ';
+$lang['proxy_report_incidents'] = 'ਘਟਨਾ';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP ਨੂੰ ਸੰਖੇਪ';
+$lang['proxy_report_malware_details'] = 'ਮਾਲਵੇਅਰ ਵੇਰਵਾ';
+$lang['proxy_report_malware'] = 'ਮਾਲਵੇਅਰ';
+$lang['proxy_report_proxy_report_dashboard'] = 'ਪਰਾਕਸੀ ਦੀ ਰਿਪੋਰਟ ਡੈਸ਼ਬੋਰਡ';
+$lang['proxy_report_site'] = 'ਸਾਈਟ';
+$lang['proxy_report_sites'] = 'ਸਾਈਟਸ';
+$lang['proxy_report_site_summary'] = 'ਸਾਈਟ ਸਮਰੀ';
+$lang['proxy_report_size'] = 'ਆਕਾਰ';
+$lang['proxy_report_top_ips'] = 'ਸਿਖਰ IPs';
+$lang['proxy_report_top_sites'] = 'ਸਿਖਰ ਸਾਈਟਸ';
+$lang['proxy_report_top_users'] = 'ਸਿਖਰ ਉਪਭੋਗਤਾ';
+$lang['proxy_report_traffic_summary'] = 'ਟਰੈਫਿਕ ਸੰਖੇਪ';
+$lang['proxy_report_user_summary'] = 'ਯੂਜ਼ਰ ਸੰਖੇਪ';
+$lang['proxy_report_warning_summary'] = 'ਚੇਤਾਵਨੀ ਸੰਖੇਪ';
+$lang['proxy_report_warnings'] = 'ਵਰਤਮਾਨ';
