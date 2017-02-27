8893582dd6a1c3c4fb5cc28b04c333bf9d297dae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:55:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Шүүлтүүр болон Proxy тайлан Хэрэв таны сүлжээний вэб хэрэглээний харах болно.';
+$lang['proxy_report_app_name'] = 'Filter болон Proxy тайлан';
+$lang['proxy_report_blacklist'] = 'Хар жагсаалт';
+$lang['proxy_report_blocked'] = 'түгжигдсэн';
+$lang['proxy_report_bytes'] = 'Bytes';
+$lang['proxy_report_cache_hits'] = 'Cache Үзсэн';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'Үзсэн';
+$lang['proxy_report_incidents'] = 'үйл явдал';
+$lang['proxy_report_ips'] = 'IP нь';
+$lang['proxy_report_ip_summary'] = 'IP тойм';
+$lang['proxy_report_malware_details'] = 'үзүүлж чадахгүй дэлгэрэнгүй';
+$lang['proxy_report_malware'] = 'үзүүлж чадахгүй';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy тайлан самбар';
+$lang['proxy_report_site'] = 'Сайтын';
+$lang['proxy_report_sites'] = 'сайтууд';
+$lang['proxy_report_site_summary'] = 'Сайтын тойм';
+$lang['proxy_report_size'] = 'Хэмжээ';
+$lang['proxy_report_top_ips'] = 'топ IP нь';
+$lang['proxy_report_top_sites'] = 'топ сайтууд';
+$lang['proxy_report_top_users'] = 'топ гишүүд';
+$lang['proxy_report_traffic_summary'] = 'Замын хөдөлгөөний тойм';
+$lang['proxy_report_user_summary'] = 'Хэрэглэгчийн тойм';
+$lang['proxy_report_warning_summary'] = 'Warning тойм';
+$lang['proxy_report_warnings'] = 'анхааруулга';
