357e4ce93f2a021d5d9f4fb50867c9ee92e8a641
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:49:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'အဆိုပါစစ်ထုတ်နဲ့ Proxy အစီရင်ခံစာသင့်ကွန်ယက်အပေါ်ကို web အသုံးပြုမှုတစ်ဦးအမြင်ပေးစွမ်းသည်။';
+$lang['proxy_report_app_name'] = 'filter နဲ့ Proxy အစီရင်ခံစာ';
+$lang['proxy_report_blacklist'] = 'အမည်ပျက်စာရင်း';
+$lang['proxy_report_blocked'] = 'ပိတ်ဆို့';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'hits';
+$lang['proxy_report_incidents'] = 'အဖြစ်အပျက်တွေ';
+$lang['proxy_report_ips'] = 'မိတ်ဖက်';
+$lang['proxy_report_ip_summary'] = 'အိုင်ပီအကျဉ်းချုပ်';
+$lang['proxy_report_malware_details'] = 'malware အသေးစိတ်';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'proxy အစီရင်ခံစာ Dashboard ကို';
+$lang['proxy_report_site'] = 'ဆိုက်ကို';
+$lang['proxy_report_sites'] = 'ဆိုဒ်များ';
+$lang['proxy_report_site_summary'] = 'ဆိုက်ကိုအကျဉ်းချုပ်';
+$lang['proxy_report_size'] = 'အရွယ်';
+$lang['proxy_report_top_ips'] = 'ထိပ်တန်းမိတ်ဖက်';
+$lang['proxy_report_top_sites'] = 'ထိပ်တန်းဆိုဒ်များ';
+$lang['proxy_report_top_users'] = 'ထိပ်တန်းအသုံးပြုသူများ';
+$lang['proxy_report_traffic_summary'] = 'ယာဉ်အသွားအလာအကျဉ်းချုပ်';
+$lang['proxy_report_user_summary'] = 'အသုံးပြုသူအကျဉ်းချုပ်';
+$lang['proxy_report_warning_summary'] = 'သတိပေးအကျဉ်းချုပ်';
+$lang['proxy_report_warnings'] = 'သတိပေးချက်များ';
