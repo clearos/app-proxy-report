8f296a55ce260558aacdf65a99b52b87e0dbb2ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:53:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Iyo na Proxy Report na-enye a anya nke web ojiji on netwọk gị.';
+$lang['proxy_report_app_name'] = 'Iyo na Proxy Report';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'Egbochiri';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'mere';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'IP Summary';
+$lang['proxy_report_malware_details'] = 'malware Details';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'size';
+$lang['proxy_report_top_ips'] = 'top IPS';
+$lang['proxy_report_top_sites'] = 'top Sites';
+$lang['proxy_report_top_users'] = 'top Ọrụ';
+$lang['proxy_report_traffic_summary'] = 'okporo ụzọ Summary';
+$lang['proxy_report_user_summary'] = 'User Summary';
+$lang['proxy_report_warning_summary'] = 'ịdọ aka ná ntị Summary';
+$lang['proxy_report_warnings'] = 'ịdọ aka ná ntị';
