305a3ecea9a5101477171f776d28b59968232ff1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:01:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'I Hlunga and Proxy Umbiko inikeza umbono ukusetshenziswa web kunethiwekhi yakho.';
+$lang['proxy_report_app_name'] = 'Hlunga bese Proxy Umbiko';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'uHla Lwesizinda Oluvinjiwe';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Yemibhalo Yehhovisi Hits';
+$lang['proxy_report_cache_percentage'] = 'Yemibhalo Yehhovisi%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'Izigameko';
+$lang['proxy_report_ips'] = 'PS';
+$lang['proxy_report_ip_summary'] = 'IP Summary';
+$lang['proxy_report_malware_details'] = 'Yekuchumana I-Malware';
+$lang['proxy_report_malware'] = 'I-Malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Umbiko Dashboard';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'Amasayithi';
+$lang['proxy_report_site_summary'] = 'ebaliwe';
+$lang['proxy_report_size'] = 'Usayizi';
+$lang['proxy_report_top_ips'] = 'PS Top';
+$lang['proxy_report_top_sites'] = 'Amasayithi Top';
+$lang['proxy_report_top_users'] = 'Abasebenzisi Top';
+$lang['proxy_report_traffic_summary'] = 'Traffic Summary';
+$lang['proxy_report_user_summary'] = 'Isifinyezo Umsebenzisi';
+$lang['proxy_report_warning_summary'] = 'Isexwayiso Summary';
+$lang['proxy_report_warnings'] = 'Izixwayiso';
