02b9034f7db5fbe85e6efddfbc2ff1c93ab6f871
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:58:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'මෙම පෙරහන සහ ප්රොක්සි වාර්තාව ඔබගේ ජාල වෙබ් භාවිතය පිළිබඳ දැක්ම සපයයි.';
+$lang['proxy_report_app_name'] = 'පෙරහන් හා ප්රොක්සි වාර්තාව';
+$lang['proxy_report_blacklist'] = 'ට තුවාලයි';
+$lang['proxy_report_blocked'] = 'ඇහිරීම';
+$lang['proxy_report_bytes'] = 'බයිට්';
+$lang['proxy_report_cache_hits'] = 'හැඹිලි වැදුම්';
+$lang['proxy_report_cache_percentage'] = 'හැඹිලි%';
+$lang['proxy_report_hits'] = 'වැදුම්';
+$lang['proxy_report_incidents'] = 'සිදුවීම්';
+$lang['proxy_report_ips'] = 'වෙන කිසිවක් නිසා නොව';
+$lang['proxy_report_ip_summary'] = 'IP සාරාංශය';
+$lang['proxy_report_malware_details'] = 'අනිෂ්ට මෘදුකාංග විස්තර';
+$lang['proxy_report_malware'] = 'අනිෂ්ට මෘදුකාංග';
+$lang['proxy_report_proxy_report_dashboard'] = 'ප්රොක්සි වාර්තාව උපකරණ පුවරුව';
+$lang['proxy_report_site'] = 'අඩවිය';
+$lang['proxy_report_sites'] = 'අඩවි';
+$lang['proxy_report_site_summary'] = 'අඩවි සාරාංශය';
+$lang['proxy_report_size'] = 'තරම';
+$lang['proxy_report_top_ips'] = 'Top ස්ථානාධිපතිවරයාට';
+$lang['proxy_report_top_sites'] = 'Top අඩවි';
+$lang['proxy_report_top_users'] = 'Top පරිශීලකයන්';
+$lang['proxy_report_traffic_summary'] = 'රථ වාහන සාරාංශය';
+$lang['proxy_report_user_summary'] = 'පරිශීලක සාරාංශය';
+$lang['proxy_report_warning_summary'] = 'අවවාදයයි සාරාංශය';
+$lang['proxy_report_warnings'] = 'අනතුරු ඇඟවීම්';
