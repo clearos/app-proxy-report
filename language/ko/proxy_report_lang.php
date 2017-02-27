65d46b94356c7d78be0e1d464dcc0350b41211db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:54:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = '필터 및 프록시 보고서는 네트워크의 웹 사용의보기를 제공합니다.';
+$lang['proxy_report_app_name'] = '필터 및 프록시 보고서';
+$lang['proxy_report_blacklist'] = '블랙리스트';
+$lang['proxy_report_blocked'] = '막힌';
+$lang['proxy_report_bytes'] = '바이트';
+$lang['proxy_report_cache_hits'] = '캐시 조회수';
+$lang['proxy_report_cache_percentage'] = '캐시 %';
+$lang['proxy_report_hits'] = '조회수';
+$lang['proxy_report_incidents'] = '사고';
+$lang['proxy_report_ips'] = 'IP를';
+$lang['proxy_report_ip_summary'] = 'IP 요약';
+$lang['proxy_report_malware_details'] = '악성 코드의 자세한 사항';
+$lang['proxy_report_malware'] = '악성 코드';
+$lang['proxy_report_proxy_report_dashboard'] = '프록시 보고서 대시 보드';
+$lang['proxy_report_site'] = '대지';
+$lang['proxy_report_sites'] = '사이트';
+$lang['proxy_report_site_summary'] = '사이트 요약';
+$lang['proxy_report_size'] = '크기';
+$lang['proxy_report_top_ips'] = '인기 IP를';
+$lang['proxy_report_top_sites'] = '상위 사이트';
+$lang['proxy_report_top_users'] = '최고 사용자';
+$lang['proxy_report_traffic_summary'] = '교통 요약';
+$lang['proxy_report_user_summary'] = '사용자 요약';
+$lang['proxy_report_warning_summary'] = '경고 요약';
+$lang['proxy_report_warnings'] = '경고';
