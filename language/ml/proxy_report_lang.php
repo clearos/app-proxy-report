2290c0a0c786491339c8dbb3741c976cd3d8ced8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:55:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ഫിൽട്ടർ പ്രോക്സി റിപ്പോർട്ട് നിങ്ങളുടെ നെറ്റ്വർക്കിൽ വെബ് ഉപയോഗം ഒരു കാഴ്ച നൽകുന്നു.';
+$lang['proxy_report_app_name'] = 'ഫിൽട്ടർ പ്രോക്സി റിപ്പോർട്ട്';
+$lang['proxy_report_blacklist'] = 'കരിമ്പട്ടിക';
+$lang['proxy_report_blocked'] = 'തടഞ്ഞ';
+$lang['proxy_report_bytes'] = 'ബൈറ്റുകൾ';
+$lang['proxy_report_cache_hits'] = 'കാഷെ ഹിറ്റുകൾ';
+$lang['proxy_report_cache_percentage'] = 'കാഷെ%';
+$lang['proxy_report_hits'] = 'ഹിറ്റുകൾ';
+$lang['proxy_report_incidents'] = 'സംഭവങ്ങളിലായി';
+$lang['proxy_report_ips'] = 'IP കൾ';
+$lang['proxy_report_ip_summary'] = 'ഐപി ചുരുക്കം';
+$lang['proxy_report_malware_details'] = 'ക്ഷുദ്രവെയർ വിശദാംശങ്ങൾ';
+$lang['proxy_report_malware'] = 'ക്ഷുദ്രവെയർ';
+$lang['proxy_report_proxy_report_dashboard'] = 'പ്രോക്സി റിപ്പോർട്ട് ഡാഷ്ബോർഡ്';
+$lang['proxy_report_site'] = 'സൈറ്റ്';
+$lang['proxy_report_sites'] = 'സൈറ്റുകൾ';
+$lang['proxy_report_site_summary'] = 'സൈറ്റ് സമ്മറി';
+$lang['proxy_report_size'] = 'വലുപ്പം';
+$lang['proxy_report_top_ips'] = 'ടോപ്പ് IP കൾ';
+$lang['proxy_report_top_sites'] = 'ടോപ്പ് സൈറ്റുകൾ';
+$lang['proxy_report_top_users'] = 'മികച്ച ഉപയോക്താക്കൾ';
+$lang['proxy_report_traffic_summary'] = 'ട്രാഫിക് ചുരുക്കം';
+$lang['proxy_report_user_summary'] = 'ഉപയോക്തൃ ചുരുക്കം';
+$lang['proxy_report_warning_summary'] = 'മുന്നറിയിപ്പ് ചുരുക്കം';
+$lang['proxy_report_warnings'] = 'നിലവിലുള്ള';
