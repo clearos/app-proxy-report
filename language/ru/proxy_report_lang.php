e9e041c0fdcc3632b8728d30030bb60ffa65e4be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:57:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Фильтр и прокси отчет обеспечивает просмотр веб-использования в вашей сети.';
+$lang['proxy_report_app_name'] = 'Фильтр и прокси-отчет';
+$lang['proxy_report_blacklist'] = 'Черный список';
+$lang['proxy_report_blocked'] = 'блокированный';
+$lang['proxy_report_bytes'] = 'Б';
+$lang['proxy_report_cache_hits'] = 'Cache Hits';
+$lang['proxy_report_cache_percentage'] = 'кэш%';
+$lang['proxy_report_hits'] = 'Хиты';
+$lang['proxy_report_incidents'] = 'Происшествия';
+$lang['proxy_report_ips'] = 'IP-адреса';
+$lang['proxy_report_ip_summary'] = 'IP Дополнительная информация';
+$lang['proxy_report_malware_details'] = 'Сведения о вредоносных программах';
+$lang['proxy_report_malware'] = 'Вредоносное';
+$lang['proxy_report_proxy_report_dashboard'] = 'Прокси-отчет Панель приборов';
+$lang['proxy_report_site'] = 'сайт';
+$lang['proxy_report_sites'] = 'Места';
+$lang['proxy_report_site_summary'] = 'Сводка по сайту';
+$lang['proxy_report_size'] = 'Размер';
+$lang['proxy_report_top_ips'] = 'Лучшие IP-адреса';
+$lang['proxy_report_top_sites'] = 'Лучшие сайты';
+$lang['proxy_report_top_users'] = 'Топ пользователей';
+$lang['proxy_report_traffic_summary'] = 'Трафик Резюме';
+$lang['proxy_report_user_summary'] = 'Резюме пользователя';
+$lang['proxy_report_warning_summary'] = 'Предупреждение Резюме';
+$lang['proxy_report_warnings'] = 'Предупреждения';
