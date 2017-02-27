a0029389acf6c694b003791aa0fb66a85e4f00d0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:52:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'A szűrő és proxy jelentés képet ad a web használatát a hálózaton.';
+$lang['proxy_report_app_name'] = 'Szűrjük le és Proxy jelentés';
+$lang['proxy_report_blacklist'] = 'Feketelista';
+$lang['proxy_report_blocked'] = 'Zárolt';
+$lang['proxy_report_bytes'] = 'bájt';
+$lang['proxy_report_cache_hits'] = 'gyorsítótár találatok';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'Találat';
+$lang['proxy_report_incidents'] = 'incidensek';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'IP-összefoglaló';
+$lang['proxy_report_malware_details'] = 'Részletek rosszindulatú';
+$lang['proxy_report_malware'] = 'Rosszindulatú';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Portál';
+$lang['proxy_report_site'] = 'Webhely';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Méret';
+$lang['proxy_report_top_ips'] = 'Top IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top felhasználók';
+$lang['proxy_report_traffic_summary'] = 'Forgalmi összesítő';
+$lang['proxy_report_user_summary'] = 'Felhasználó Összefoglaló';
+$lang['proxy_report_warning_summary'] = 'Figyelmeztetés Összefoglaló';
+$lang['proxy_report_warnings'] = 'figyelmeztetések';
