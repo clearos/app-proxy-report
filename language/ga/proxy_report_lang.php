cab76e1beb2ef4fa150addf17fc5082cfc8b0218
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:52:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Soláthraíonn an Scagaire agus Tuarascáil Proxy d&#39;fhonn úsáide gréasáin ar do líonra.';
+$lang['proxy_report_app_name'] = 'Scagaire agus Tuarascáil Proxy';
+$lang['proxy_report_blacklist'] = 'blacklist';
+$lang['proxy_report_blocked'] = 'Bactha';
+$lang['proxy_report_bytes'] = 'bearta';
+$lang['proxy_report_cache_hits'] = 'Hits Cache';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'Teagmhais';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'Achoimre IP';
+$lang['proxy_report_malware_details'] = 'Mionsonraí malware';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Tuarascáil Painéal na nIonstraimí';
+$lang['proxy_report_site'] = 'suíomh';
+$lang['proxy_report_sites'] = 'Láithreáin';
+$lang['proxy_report_site_summary'] = 'Achoimre ar an suíomh';
+$lang['proxy_report_size'] = 'méid';
+$lang['proxy_report_top_ips'] = 'IPs barr';
+$lang['proxy_report_top_sites'] = 'Láithreáin barr';
+$lang['proxy_report_top_users'] = 'úsáideoirí';
+$lang['proxy_report_traffic_summary'] = 'Achoimre tráchta';
+$lang['proxy_report_user_summary'] = 'Úsáideoir Achoimre';
+$lang['proxy_report_warning_summary'] = 'Achoimre rabhadh';
+$lang['proxy_report_warnings'] = 'Rabhaidh';
