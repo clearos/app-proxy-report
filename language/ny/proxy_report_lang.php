093ee0b3e7aea3d8999b8243c2f665c104102530
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:50:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Fyuluta ndi Report tidzakulowereni amapereka maganizo a wakagwiritsidwe intaneti pa Intaneti wanu.';
+$lang['proxy_report_app_name'] = 'Zosefera ndi Report tidzakulowereni';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'oletsedwa';
+$lang['proxy_report_bytes'] = 'mabayiti';
+$lang['proxy_report_cache_hits'] = 'posungira kumenya';
+$lang['proxy_report_cache_percentage'] = 'posungira%';
+$lang['proxy_report_hits'] = 'kumenya';
+$lang['proxy_report_incidents'] = 'zochitika';
+$lang['proxy_report_ips'] = 'IPS';
+$lang['proxy_report_ip_summary'] = 'IP Chidule';
+$lang['proxy_report_malware_details'] = 'Tsatanetsatane yaumbanda';
+$lang['proxy_report_malware'] = 'yaumbanda';
+$lang['proxy_report_proxy_report_dashboard'] = 'Tidzakulowereni Report lakutsogolo';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'Site Chidule';
+$lang['proxy_report_size'] = 'kukula';
+$lang['proxy_report_top_ips'] = 'Top IPS';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top Ogwiritsa';
+$lang['proxy_report_traffic_summary'] = 'magalimoto Chidule';
+$lang['proxy_report_user_summary'] = 'Chidule wosuta';
+$lang['proxy_report_warning_summary'] = 'chenjezo Chidule';
+$lang['proxy_report_warnings'] = 'machenjezo';
