ed5dab9df99c82d8cc520e8ed3501729e658a682
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:00:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Ang Filter at Proxy Report ay nagbibigay ng isang view ng paggamit ng web sa iyong network.';
+$lang['proxy_report_app_name'] = 'Salain at Proxy Report';
+$lang['proxy_report_blacklist'] = 'talaang-itim';
+$lang['proxy_report_blocked'] = 'Na-block';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'insidente';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP Buod';
+$lang['proxy_report_malware_details'] = 'malware Detalye';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Isumbong Dashboard';
+$lang['proxy_report_site'] = 'Lugar';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'Buod site';
+$lang['proxy_report_size'] = 'laki';
+$lang['proxy_report_top_ips'] = 'Top IPs';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Mga Nangungunang Tagagamit';
+$lang['proxy_report_traffic_summary'] = 'Buod Traffic';
+$lang['proxy_report_user_summary'] = 'Buod User';
+$lang['proxy_report_warning_summary'] = 'babala Buod';
+$lang['proxy_report_warnings'] = 'babala';
