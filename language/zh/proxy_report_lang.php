b4904f27cd881f2b5436682411aabde332f34151
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:50:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = '过滤器和代理报告提供网络使用的网络上的看法。';
+$lang['proxy_report_app_name'] = '过滤和代理报告';
+$lang['proxy_report_blacklist'] = '黑名单';
+$lang['proxy_report_blocked'] = '受阻';
+$lang['proxy_report_bytes'] = '字节';
+$lang['proxy_report_cache_hits'] = '高速缓存命中';
+$lang['proxy_report_cache_percentage'] = '高速缓存％';
+$lang['proxy_report_hits'] = '点击';
+$lang['proxy_report_incidents'] = '事故';
+$lang['proxy_report_ips'] = 'IP地址';
+$lang['proxy_report_ip_summary'] = 'IP摘要';
+$lang['proxy_report_malware_details'] = '恶意软件详细信息';
+$lang['proxy_report_malware'] = '恶意软件';
+$lang['proxy_report_proxy_report_dashboard'] = '代理仪表板报告';
+$lang['proxy_report_site'] = '现场';
+$lang['proxy_report_sites'] = '网站';
+$lang['proxy_report_site_summary'] = '站点摘要';
+$lang['proxy_report_size'] = '尺寸';
+$lang['proxy_report_top_ips'] = '顶级的IP';
+$lang['proxy_report_top_sites'] = '热门网站';
+$lang['proxy_report_top_users'] = '用户排行';
+$lang['proxy_report_traffic_summary'] = '流量总结';
+$lang['proxy_report_user_summary'] = '用户摘要';
+$lang['proxy_report_warning_summary'] = '警告摘要';
+$lang['proxy_report_warnings'] = '警告';
