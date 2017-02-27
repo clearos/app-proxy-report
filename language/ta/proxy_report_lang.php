60f0a56078d006800683ef0fcb87f5d790ecb22a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:59:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'வடிகட்டி மற்றும் பதிலாள் அறிக்கை உங்கள் பிணையத்தில் வலை பயன்பாடு ஒரு காட்சி அளிக்கிறது.';
+$lang['proxy_report_app_name'] = 'வடிகட்டி மற்றும் பதிலாள் அறிக்கை';
+$lang['proxy_report_blacklist'] = 'பிளாக்லிஸ்ட்';
+$lang['proxy_report_blocked'] = 'தடுக்கப்பட்ட';
+$lang['proxy_report_bytes'] = 'பைட்ஸ்';
+$lang['proxy_report_cache_hits'] = 'கேச் ஹிட்ஸ்';
+$lang['proxy_report_cache_percentage'] = 'கேச்%';
+$lang['proxy_report_hits'] = 'ஹிட்ஸ்';
+$lang['proxy_report_incidents'] = 'சம்பவங்கள்';
+$lang['proxy_report_ips'] = 'ஐபிஎஸ்';
+$lang['proxy_report_ip_summary'] = 'ஐபி சுருக்கம்';
+$lang['proxy_report_malware_details'] = 'மால்வேர் விபரங்கள்';
+$lang['proxy_report_malware'] = 'மால்வேர்';
+$lang['proxy_report_proxy_report_dashboard'] = 'பதிலாள் அறிக்கை டேஷ்போர்டில்';
+$lang['proxy_report_site'] = 'தள';
+$lang['proxy_report_sites'] = 'தளங்கள்';
+$lang['proxy_report_site_summary'] = 'தளச் சுருக்கம்';
+$lang['proxy_report_size'] = 'அளவு';
+$lang['proxy_report_top_ips'] = 'சிறந்த ஐபிஎஸ்';
+$lang['proxy_report_top_sites'] = 'சிறந்த தளங்கள்';
+$lang['proxy_report_top_users'] = 'சிறந்த பயனர்கள்';
+$lang['proxy_report_traffic_summary'] = 'போக்குவரத்து சுருக்கம்';
+$lang['proxy_report_user_summary'] = 'பயனர் சுருக்கம்';
+$lang['proxy_report_warning_summary'] = 'எச்சரிக்கை சுருக்கம்';
+$lang['proxy_report_warnings'] = 'எச்சரிக்கைகள்';
