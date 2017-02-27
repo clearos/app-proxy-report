bdf29804c5493a1db924ba17c142d03b8c9a9ba2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:01:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Mae&#39;r Filter ac Adroddiad Proxy yn rhoi darlun o ddefnydd ar y we ar eich rhwydwaith.';
+$lang['proxy_report_app_name'] = 'Filter ac Adroddiad thrwy Ddirprwy';
+$lang['proxy_report_blacklist'] = 'Rhestr Ddu';
+$lang['proxy_report_blocked'] = 'wedi&#39;u Rhwystro';
+$lang['proxy_report_bytes'] = 'beitiau';
+$lang['proxy_report_cache_hits'] = 'Ymweliadau cache';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'hits';
+$lang['proxy_report_incidents'] = 'digwyddiadau';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'Crynodeb IP';
+$lang['proxy_report_malware_details'] = 'Manylion malware';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Adroddiad Proxy Dashboard';
+$lang['proxy_report_site'] = 'safle';
+$lang['proxy_report_sites'] = 'safleoedd';
+$lang['proxy_report_site_summary'] = 'Crynodeb o safle';
+$lang['proxy_report_size'] = 'maint';
+$lang['proxy_report_top_ips'] = 'IPs Top';
+$lang['proxy_report_top_sites'] = 'Safleoedd Top';
+$lang['proxy_report_top_users'] = 'Defnyddwyr Top';
+$lang['proxy_report_traffic_summary'] = 'Crynodeb traffig';
+$lang['proxy_report_user_summary'] = 'Crynodeb defnyddiwr';
+$lang['proxy_report_warning_summary'] = 'rhybudd Crynodeb';
+$lang['proxy_report_warnings'] = 'rhybuddion';
