2b7ac1d075411cf0f1accb66280027db61f3f82b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:53:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'フィルターおよびプロキシレポートは、ネットワーク上のWeb利用状況のビューを提供します。';
+$lang['proxy_report_app_name'] = 'フィルタおよびプロキシレポート';
+$lang['proxy_report_blacklist'] = 'ブラックリスト';
+$lang['proxy_report_blocked'] = 'ブロックされました';
+$lang['proxy_report_bytes'] = 'バイト';
+$lang['proxy_report_cache_hits'] = 'キャッシュ・ヒット';
+$lang['proxy_report_cache_percentage'] = 'キャッシュ％';
+$lang['proxy_report_hits'] = 'ヒット';
+$lang['proxy_report_incidents'] = 'インシデント';
+$lang['proxy_report_ips'] = 'IPアドレス';
+$lang['proxy_report_ip_summary'] = 'IPの概要';
+$lang['proxy_report_malware_details'] = 'マルウェアの詳細';
+$lang['proxy_report_malware'] = 'マルウェア';
+$lang['proxy_report_proxy_report_dashboard'] = 'プロキシレポートダッシュボード';
+$lang['proxy_report_site'] = 'サイト';
+$lang['proxy_report_sites'] = 'サイト';
+$lang['proxy_report_site_summary'] = 'サイトサマリー';
+$lang['proxy_report_size'] = 'サイズ';
+$lang['proxy_report_top_ips'] = 'トップIPアドレス';
+$lang['proxy_report_top_sites'] = 'トップサイト';
+$lang['proxy_report_top_users'] = 'トップユーザー';
+$lang['proxy_report_traffic_summary'] = 'トラフィック要約';
+$lang['proxy_report_user_summary'] = 'ユーザーの概要';
+$lang['proxy_report_warning_summary'] = '警告の概要';
+$lang['proxy_report_warnings'] = '警告';
