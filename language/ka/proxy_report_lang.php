ea83ee19e4a55d69716ce00f57a519efb70b0539
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:53:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'ფილტრი და Proxy ანგარიში უზრუნველყოფს ხედი ვებ გამოყენების თქვენს ქსელში.';
+$lang['proxy_report_app_name'] = 'ფილტრი და Proxy ანგარიში';
+$lang['proxy_report_blacklist'] = 'blacklist';
+$lang['proxy_report_blocked'] = 'დაბლოკილ';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Cache ნანახია';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'ნანახია';
+$lang['proxy_report_incidents'] = 'ინციდენტების';
+$lang['proxy_report_ips'] = 'IP მისამართები';
+$lang['proxy_report_ip_summary'] = 'IP რეზიუმე';
+$lang['proxy_report_malware_details'] = 'malware დეტალები';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy ანგარიში Dashboard';
+$lang['proxy_report_site'] = 'საიტის';
+$lang['proxy_report_sites'] = 'საიტები';
+$lang['proxy_report_site_summary'] = 'საიტის რეზიუმე';
+$lang['proxy_report_size'] = 'ზომა';
+$lang['proxy_report_top_ips'] = 'ყველაზე IP მისამართები';
+$lang['proxy_report_top_sites'] = 'ყველაზე საიტები';
+$lang['proxy_report_top_users'] = 'TOP მომხმარებლები';
+$lang['proxy_report_traffic_summary'] = 'Traffic რეზიუმე';
+$lang['proxy_report_user_summary'] = 'მომხმარებელი რეზიუმე';
+$lang['proxy_report_warning_summary'] = 'გაფრთხილება რეზიუმე';
+$lang['proxy_report_warnings'] = 'გაფრთხილებები';
