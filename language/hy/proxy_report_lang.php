4671ec89848792514a7bd0b9dcd73b1f46b85c4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:48:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'The Filter եւ Proxy զեկույցը ապահովում է տեսակետը վեբ օգտագործման վրա ձեր ցանցը.';
+$lang['proxy_report_app_name'] = 'Զտել եւ Proxy հաշվետվություն';
+$lang['proxy_report_blacklist'] = 'սլ ցուցակ';
+$lang['proxy_report_blocked'] = 'Արգելափակված';
+$lang['proxy_report_bytes'] = 'բայթ';
+$lang['proxy_report_cache_hits'] = 'Cache Հիթեր';
+$lang['proxy_report_cache_percentage'] = 'Cache%';
+$lang['proxy_report_hits'] = 'հիթեր';
+$lang['proxy_report_incidents'] = 'Պատահարներ';
+$lang['proxy_report_ips'] = 'IP- ներ';
+$lang['proxy_report_ip_summary'] = 'IP Ամփոփում';
+$lang['proxy_report_malware_details'] = 'չարամիտ Մանրամասներ';
+$lang['proxy_report_malware'] = 'չարամիտ';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy հաշվետվություն Dashboard';
+$lang['proxy_report_site'] = 'կայքի';
+$lang['proxy_report_sites'] = 'Կայքեր';
+$lang['proxy_report_site_summary'] = 'կայքի Summary';
+$lang['proxy_report_size'] = 'չափ';
+$lang['proxy_report_top_ips'] = 'Ամենաշատ IP- ներ';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Ամենաշատ Users';
+$lang['proxy_report_traffic_summary'] = 'Երթեւեկության Ամփոփում';
+$lang['proxy_report_user_summary'] = 'Օգտվող Ամփոփում';
+$lang['proxy_report_warning_summary'] = 'Զգուշացում Ամփոփում';
+$lang['proxy_report_warnings'] = 'նախազգուշացումները';
