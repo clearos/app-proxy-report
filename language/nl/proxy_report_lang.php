a3beaf0a3fccc95e4c1151f0fbdb280bf3062ce9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:56:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'De Filter en Proxy verslag geeft een weergave van het gebruik van internet op uw netwerk.';
+$lang['proxy_report_app_name'] = 'Filteren en Proxy Report';
+$lang['proxy_report_blacklist'] = 'blacklist';
+$lang['proxy_report_blocked'] = 'geblokkeerd';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'incidenten';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP Samenvatting';
+$lang['proxy_report_malware_details'] = 'malware Details';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'plaats';
+$lang['proxy_report_sites'] = 'sites';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Grootte';
+$lang['proxy_report_top_ips'] = 'Top IPs';
+$lang['proxy_report_top_sites'] = 'Top sites';
+$lang['proxy_report_top_users'] = 'Top Gebruikers';
+$lang['proxy_report_traffic_summary'] = 'verkeer Samenvatting';
+$lang['proxy_report_user_summary'] = 'gebruiker Samenvatting';
+$lang['proxy_report_warning_summary'] = 'waarschuwing Samenvatting';
+$lang['proxy_report_warnings'] = 'waarschuwingen';
