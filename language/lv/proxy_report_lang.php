d61a69518c506c0d11a99b3156fb4f188700b9fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:55:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filtru un Proxy ziņojums sniedz pārskatu par interneta izmantošanu jūsu tīklā.';
+$lang['proxy_report_app_name'] = 'Filter un Proxy pārskats';
+$lang['proxy_report_blacklist'] = 'ierakstīt melnajā sarakstā';
+$lang['proxy_report_blocked'] = 'bloķēts';
+$lang['proxy_report_bytes'] = 'baiti';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'cache%';
+$lang['proxy_report_hits'] = 'hits';
+$lang['proxy_report_incidents'] = 'starpgadījumi';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'IP kopsavilkums';
+$lang['proxy_report_malware_details'] = 'Malware Details';
+$lang['proxy_report_malware'] = 'Malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy ziņojums Dashboard';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Izmērs';
+$lang['proxy_report_top_ips'] = 'Top IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top lietotāji';
+$lang['proxy_report_traffic_summary'] = 'satiksmes kopsavilkums';
+$lang['proxy_report_user_summary'] = 'lietotājs kopsavilkums';
+$lang['proxy_report_warning_summary'] = 'Brīdinājums kopsavilkums';
+$lang['proxy_report_warnings'] = 'brīdinājumi';
