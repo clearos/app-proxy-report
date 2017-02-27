552ede2a15a3188c8db47842ac4e3159008d1540
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:49:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filter i Proxy Izvještaj pruža pogled korištenja web na vašoj mreži.';
+$lang['proxy_report_app_name'] = 'Filter i Proxy izvještaj';
+$lang['proxy_report_blacklist'] = 'Blacklist';
+$lang['proxy_report_blocked'] = 'blokiran';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'cache Pogodaka';
+$lang['proxy_report_cache_percentage'] = 'Skladište %';
+$lang['proxy_report_hits'] = 'Pogodaka';
+$lang['proxy_report_incidents'] = 'incidenti';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'IP Pregled';
+$lang['proxy_report_malware_details'] = 'malware Detalji';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'site';
+$lang['proxy_report_sites'] = 'Sites';
+$lang['proxy_report_site_summary'] = 'site Summary';
+$lang['proxy_report_size'] = 'Veličina';
+$lang['proxy_report_top_ips'] = 'Top IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top korisnici';
+$lang['proxy_report_traffic_summary'] = 'Traffic Pregled';
+$lang['proxy_report_user_summary'] = 'korisnik Pregled';
+$lang['proxy_report_warning_summary'] = 'Upozorenje Pregled';
+$lang['proxy_report_warnings'] = 'upozorenja';
