34fa3401fbfeef9d1ed956df5037b3f6794906ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:48:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filter eta Proxy txostena web erabilera ikuspegi bat zure sarean eskaintzen.';
+$lang['proxy_report_app_name'] = 'Iragazi eta Proxy txostena';
+$lang['proxy_report_blacklist'] = 'Zerrenda beltza';
+$lang['proxy_report_blocked'] = 'blokeatutako';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'Cache Hits';
+$lang['proxy_report_cache_percentage'] = '% Cache';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'Gorabeherak';
+$lang['proxy_report_ips'] = 'IPak';
+$lang['proxy_report_ip_summary'] = 'IP Laburpena';
+$lang['proxy_report_malware_details'] = 'malware xehetasunak';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy txostena arbela';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'guneak';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Tamaina';
+$lang['proxy_report_top_ips'] = 'Top IPak';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top erabiltzaileak';
+$lang['proxy_report_traffic_summary'] = 'Trafiko Laburpena';
+$lang['proxy_report_user_summary'] = 'Erabiltzaile Laburpena';
+$lang['proxy_report_warning_summary'] = 'Abisua Laburpena';
+$lang['proxy_report_warnings'] = 'Abisu';
