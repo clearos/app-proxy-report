624903deedf1c8c62063c8860273c560965159ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:49:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'El filtre i el proxy informe proporciona una vista d&#39;ús de la web a la xarxa.';
+$lang['proxy_report_app_name'] = 'Es filtra i el proxy informe';
+$lang['proxy_report_blacklist'] = 'llista negra';
+$lang['proxy_report_blocked'] = 'obstruït';
+$lang['proxy_report_bytes'] = 'bytes';
+$lang['proxy_report_cache_hits'] = 'colpeja memòria cau';
+$lang['proxy_report_cache_percentage'] = 'cau%';
+$lang['proxy_report_hits'] = 'Accessos';
+$lang['proxy_report_incidents'] = 'incidents';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'Resum de IP';
+$lang['proxy_report_malware_details'] = 'Detalls de malware';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy informe de panell';
+$lang['proxy_report_site'] = 'lloc';
+$lang['proxy_report_sites'] = 'llocs';
+$lang['proxy_report_site_summary'] = 'Resum de la pàgina';
+$lang['proxy_report_size'] = 'mida';
+$lang['proxy_report_top_ips'] = 'Top IPs';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top usuaris';
+$lang['proxy_report_traffic_summary'] = 'Resum del trànsit';
+$lang['proxy_report_user_summary'] = 'Resum de l&#39;usuari';
+$lang['proxy_report_warning_summary'] = 'advertència Resum';
+$lang['proxy_report_warnings'] = 'advertiments';
