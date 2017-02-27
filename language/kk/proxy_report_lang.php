38e71e9012b8faf6fe64da3bfe7e5c87a7897d61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:53:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Сүзгі және Proxy есеп желідегі веб пайдалану көрінісін береді.';
+$lang['proxy_report_app_name'] = 'Сүзгі және Прокси есеп';
+$lang['proxy_report_blacklist'] = 'Қара тізімге';
+$lang['proxy_report_blocked'] = 'бұғатталған';
+$lang['proxy_report_bytes'] = 'Bytes';
+$lang['proxy_report_cache_hits'] = 'Кэш Hits';
+$lang['proxy_report_cache_percentage'] = 'Кэш%';
+$lang['proxy_report_hits'] = 'Hits';
+$lang['proxy_report_incidents'] = 'Оқиға';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP Резюме';
+$lang['proxy_report_malware_details'] = 'Malware Толығырақ';
+$lang['proxy_report_malware'] = 'Malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Прокси есеп Dashboard';
+$lang['proxy_report_site'] = 'Сайт';
+$lang['proxy_report_sites'] = 'сайттар';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Өлшем';
+$lang['proxy_report_top_ips'] = 'Жоғары IPs';
+$lang['proxy_report_top_sites'] = 'Жоғары сайттар';
+$lang['proxy_report_top_users'] = 'Жоғары Пайдаланушылар';
+$lang['proxy_report_traffic_summary'] = 'Traffic Резюме';
+$lang['proxy_report_user_summary'] = 'пайдаланушы Резюме';
+$lang['proxy_report_warning_summary'] = 'Ескерту Жиынтық';
+$lang['proxy_report_warnings'] = 'Ескертулер';
