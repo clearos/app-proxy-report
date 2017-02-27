8670c7238d4d6d2f8394b3957146a6db0ed99424
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:50:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Filtr a Report Proxy poskytuje pohled na webové použití ve vaší síti.';
+$lang['proxy_report_app_name'] = 'Filtrovat a Report Proxy';
+$lang['proxy_report_blacklist'] = 'Černá listina';
+$lang['proxy_report_blocked'] = 'Blokováno';
+$lang['proxy_report_bytes'] = 'bajtů';
+$lang['proxy_report_cache_hits'] = 'Cache Hits';
+$lang['proxy_report_cache_percentage'] = 'Mezipaměti %';
+$lang['proxy_report_hits'] = 'hity';
+$lang['proxy_report_incidents'] = 'incidenty';
+$lang['proxy_report_ips'] = 'IPs';
+$lang['proxy_report_ip_summary'] = 'IP Shrnutí';
+$lang['proxy_report_malware_details'] = 'malware Podrobnosti';
+$lang['proxy_report_malware'] = 'malware';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'Site';
+$lang['proxy_report_sites'] = 'Stránky';
+$lang['proxy_report_site_summary'] = 'Site Summary';
+$lang['proxy_report_size'] = 'Velikost';
+$lang['proxy_report_top_ips'] = 'Nejlepší IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Top uživatelé';
+$lang['proxy_report_traffic_summary'] = 'dopravní Souhrn';
+$lang['proxy_report_user_summary'] = 'Shrnutí Uživatel';
+$lang['proxy_report_warning_summary'] = 'Varování Souhrn';
+$lang['proxy_report_warnings'] = 'varování';
