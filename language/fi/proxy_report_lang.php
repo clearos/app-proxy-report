cc65847a47b0b46c7917530ccdc85e4166f9121a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 00:51:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/proxy_report_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['proxy_report_app_description'] = 'Suodatin ja Proxy Raportti tarjoaa näkymän web käyttö verkossa.';
+$lang['proxy_report_app_name'] = 'Suodata ja Proxy Report';
+$lang['proxy_report_blacklist'] = 'musta lista';
+$lang['proxy_report_blocked'] = 'tukossa';
+$lang['proxy_report_bytes'] = 'tavua';
+$lang['proxy_report_cache_hits'] = 'cache Hits';
+$lang['proxy_report_cache_percentage'] = 'Kätkö %';
+$lang['proxy_report_hits'] = 'Osumien';
+$lang['proxy_report_incidents'] = 'vaaratilanteet';
+$lang['proxy_report_ips'] = 'IP';
+$lang['proxy_report_ip_summary'] = 'IP Yhteenveto';
+$lang['proxy_report_malware_details'] = 'Lisätietoja haittaohjelmista';
+$lang['proxy_report_malware'] = 'haittaohjelmat';
+$lang['proxy_report_proxy_report_dashboard'] = 'Proxy Report Dashboard';
+$lang['proxy_report_site'] = 'paikka';
+$lang['proxy_report_sites'] = 'Sivustot';
+$lang['proxy_report_site_summary'] = 'site Summary';
+$lang['proxy_report_size'] = 'Koko';
+$lang['proxy_report_top_ips'] = 'Top IP';
+$lang['proxy_report_top_sites'] = 'Top Sites';
+$lang['proxy_report_top_users'] = 'Suosituimmat käyttäjät';
+$lang['proxy_report_traffic_summary'] = 'liikenne Yhteenveto';
+$lang['proxy_report_user_summary'] = 'Käyttäjä Yhteenveto';
+$lang['proxy_report_warning_summary'] = 'Varoitus Yhteenveto';
+$lang['proxy_report_warnings'] = 'varoitukset';
